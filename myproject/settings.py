fdc030c64a32e4499dcf5bfa3117bb5e1d155c16
Kieren Gill <kierengill2000@gmail.com>
2021-03-30 00:34:51 -0400

    Adding files

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1bk6bhk6vz#tf3dypn!f83d79+r1u(jj&^s^2un&6q(#q17%b&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sunway-internship-project.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
